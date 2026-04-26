2e9f8851cbe3f2eaa1ecc567e9946fb95dcf0e76
umeshunique <umeshunique003@gmail.com>
2026-04-26 15:27:11 +0530

    feat: add script from local repo

--- scripts/new-script.sql
+++ scripts/new-script.sql
@@ -1,5 +1,7 @@
--- Write your SQL script heresxcxcxcccsdsdsd
-
-
-select * from country_master; 
- umesh kumar mahesah kkumar vangari 
+CREATE TABLE `app_tier` (
+  `app_tier_id` char(36) NOT NULL,
+  `app_tier_name` varchar(255) NOT NULL,
+  `app_tier_description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`app_tier_id`),
+  UNIQUE KEY `app_tier_name` (`app_tier_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
